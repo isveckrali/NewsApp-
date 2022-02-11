6c01f7a1cd4919dba71ba4515d706fb1d18fad8b
mehmetcanseyhan <13541515.mehmetcan.seyhan@gmail.com>
2022-02-11 14:20:47 +0100

    Initial Commit

---
+++ AppMM/AppMMApp.swift
@@ -0,0 +1,20 @@
+//
+//  AppMMApp.swift
+//  AppMM
+//
+//  Created by Mehmet Can Seyhan on 2022-02-11.
+//
+
+import SwiftUI
+
+@main
+struct AppMMApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ AppMMUITests/AppMMUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  AppMMUITestsLaunchTests.swift
+//  AppMMUITests
+//
+//  Created by Mehmet Can Seyhan on 2022-02-11.
+//
+
+import XCTest
+
+class AppMMUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
